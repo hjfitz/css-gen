42f94352689b278768446fbeb41b4b66e76057b0
Harry <hfitz83@gmail.com>
2017-08-30 20:08:51 +0100

    init

---
+++ server-controllers/css-gen/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const cssApi = express.Router();
+
+const loadFile = () => {
+
+};
+
+cssApi.get('/', (req, res) => {
+
+});
+
+module.exports = cssApi;
